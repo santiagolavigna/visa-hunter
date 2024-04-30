0ae525f62e4eb04fa656605c790f931f3d2e05aa
Santiago Lavigna <santiagolavigna1@gmail.com>
2024-04-30 08:27:19 -0300

    Initial commit

---
+++ src/components/features.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export const Features = (props) => {
+  console.log(props.traductions)
+  return (
+    <div id="features" className="text-center">
+      <div className="container">
+        <div className="col-md-10 col-md-offset-1 section-title">
+          <h2>{props.traductions ? props.traductions.features : "Loading..."}</h2>
+        </div>
+        <div className="row">
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.title}-${i}`} className="col-xs-6 col-md-3">
+                  {" "}
+                  <i className={d.icon}></i>
+                  <h3>{d.title}</h3>
+                  <p>{d.text}</p>
+                </div>
+              ))
+            : "Loading..."}
+        </div>
+      </div>
+    </div>
+  );
+};
